09d332ce9e36cda54088ae12094c8a29768c2d94
dua74 <helaldua74@outlook.com>
2025-05-15 12:41:09 +0300

    Initial Push

---
+++ Ecom.infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Ecom.Core.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x=>x.Description).IsRequired();
+            builder.Property(x=>x.Price).HasColumnType("decimal(18,2)");
+            builder.HasData(new Product
+            {
+                Id = 1,
+                Name = "TEST",
+                Description = "TEST.DESC",
+                Price = 100,
+                CategoryId = 1
+            });
+        }
+    }
+}
